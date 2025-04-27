f8e3a1856a71f59350367140dae4fed69d8ebd35
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-27 14:47:52 +0200

    Adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import {Hono} from 'hono'
 import articles from './articles'
 import stripeEndpoint from "./stripe";
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+app.use('*', cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "OPTIONS"]
+}))
 
 app.get('/', (c) => c.text("Hello Cloudflare Workers!"))
 app.route('/articles', articles);
